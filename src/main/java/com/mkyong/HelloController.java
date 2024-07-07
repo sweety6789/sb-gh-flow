a9e2c0a8a808abea26b0c02ba9fcb9e197e70838
ash <stardom374@gmail.com>
2024-07-07 20:38:00 +0530

    solved few issues

--- src/main/java/com/mkyong/HelloController.java
+++ src/main/java/com/mkyong/HelloController.java
@@ -1,13 +1,11 @@
 package com.mkyong;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @RequestMapping("/")
-    String hello() {
+    public String hello() {
         return "Hello World, Spring Boot!";
     }
 
